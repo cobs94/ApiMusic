1ac70a7ec6fdc0684b55fb85750256356c779044
spaceterrateam <daniel_hung_apps1m1617@cev.com>
2018-01-16 12:05:19 +0100

    cambio mysql:host segunda vez

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -6,7 +6,7 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=http://81.169.234.32;dbname=diego',
+			'dsn'        => 'mysql:host=localhost;dbname=diego',
 			'username'   => 'diego',
 			'password'   => 'S09KKjrvxwvwGEfd',
 		),
