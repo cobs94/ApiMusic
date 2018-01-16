1819a4ab678aac6645aaacd22c1baa2eaa7d522a
spaceterrateam <daniel_hung_apps1m1617@cev.com>
2018-01-16 11:55:27 +0100

    cambiada ip a base de datos diego

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -6,7 +6,7 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=api_Spaceterra',
+			'dsn'        => 'mysql:host=81.169.234.32;dbname=diego',
 			'username'   => 'alumnos',
 			'password'   => 'cev',
 		),
