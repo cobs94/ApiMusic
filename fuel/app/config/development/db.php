6721464800bf223dd56eb0c70747a9ba9fb43d15
spaceterrateam <daniel_hung_apps1m1617@cev.com>
2018-01-16 11:59:38 +0100

    cambiada nombre y contraseña para prueba

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -7,8 +7,8 @@ return array(
 	'default' => array(
 		'connection'  => array(
 			'dsn'        => 'mysql:host=81.169.234.32;dbname=diego',
-			'username'   => 'alumnos',
-			'password'   => 'cev',
+			'username'   => 'diego',
+			'password'   => 'S09KKjrvxwvwGEfd',
 		),
 	),
 );
