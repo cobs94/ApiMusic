4e83b0ee24aa7be192df6ad8f239f79e54133e8c
TU NOMBRE <TU EMAIL en github>
2018-01-22 14:31:26 +0100

    arreglo error editable

--- fuel/app/migrations/003_esquemas.php
+++ fuel/app/migrations/003_esquemas.php
@@ -9,7 +9,7 @@ class esquemas
         \DBUtil::create_table('esquemas', array(
                 'id' => array('type' => 'int', 'constraint' => 11, 'auto_increment' => true),
                 'name' => array('type' => 'varchar', 'constraint' => 100),
-                'editable' => array('type' => 'bool', 'constraint' => 100),
+                'editable' => array('type' => 'bool'),
                 'ranking' => array('type' => 'varchar', 'constraint' => 100),
             ), array('id'), false, 'InnoDB', 'utf8_unicode_ci'
         );   
