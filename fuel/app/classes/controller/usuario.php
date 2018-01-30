f6a2ec39d1dba1d9964262a42d3c246ef906527e
TU NOMBRE <TU EMAIL en github>
2018-01-30 11:33:12 +0100

    mergados proyecto alex y diego

--- fuel/app/classes/controller/usuario.php
+++ fuel/app/classes/controller/usuario.php
@@ -87,7 +87,7 @@ class Controller_usuario extends Controller_Rest
     }
 }
 
-public function get_authorization(){
+public function get_allUsers(){
     $jwt = apache_request_headers()['Authorization'];
 
     $tokenDecode = JWT::decode($jwt, $this->key , array('HS256'));
@@ -134,7 +134,7 @@ public function post_modifyPassword(){
     } 
 }
 
-public function post_deleteUser(){
+public function post_deleteOwnUser(){
     $jwt = apache_request_headers()['Authorization'];
     try{
         if(!empty($jwt)){
@@ -153,7 +153,36 @@ public function post_deleteUser(){
 
                 $this->Mensaje('200', 'usuario borrado', $BDuser);
             } else {
-                $this->Mensaje('400', 'usuario incorrecto', $input['username']);
+                $this->Mensaje('400', 'usuario invalido', $input['username']);
+            }
+        } else {
+            $this->Mensaje('400', 'token vacio', $jwt);
+        }
+    }catch(Exception $e) {
+        $this->Mensaje('500', 'Error de verificacion', "aprender a programar");
+    } 
+}
+
+public function post_deleteUser(){
+    $jwt = apache_request_headers()['Authorization'];
+    //print($_POST["id"]);
+    try{
+        if(!empty($jwt)){
+
+            $id = $_POST["id"];
+            
+            $BDuser = Model_Usuarios::find('first', array(
+                'where' => array(
+                    array('id', $id)
+                    ),
+                ));
+            if($BDuser != null){
+
+                $BDuser->delete();
+
+                $this->Mensaje('200', 'usuario borrado', $BDuser);
+            } else {
+                $this->Mensaje('400', 'usuario invalido', $input['username']);
             }
         } else {
             $this->Mensaje('400', 'token vacio', $jwt);
